1965bc0fe8d4be31d807a2457f92c3a858801694
Steven Hall <steven.hall@goldenhippo.com>
2023-06-19 05:05:38 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Steven Hall | Full Stack Developer',
-  description: '//TODO: Add description',
+  description: 'Steven Hall is a full stack developer with a passion for building great products.',
+  image: 'https://avatars.githubusercontent.com/u/29719881?v=4',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
